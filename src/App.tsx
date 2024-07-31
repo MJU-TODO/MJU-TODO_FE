570fb647a52d6e15aa7d100b1aaf14742864a3a8
김민준 <alswns1285@gmail.com>
2024-07-31 19:57:07 +0900

    feat: 헤더 추가 및 outlet 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import Header from '@layouts/Header';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Outlet } from 'react-router-dom';
 
 const queryClient = new QueryClient();
 
 export default function App() {
-  return <QueryClientProvider client={queryClient}>App</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Header />
+      <Outlet />
+    </QueryClientProvider>
+  );
 }
